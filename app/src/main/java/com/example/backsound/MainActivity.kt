8420236a424cef0ee379b7ab8a2a9add7fe2beef
ATG-5222 <A01275222@tec.mx>
2022-10-20 12:43:56 -0500

    Background images in CV

--- app/src/main/java/com/example/backsound/MainActivity.kt
+++ app/src/main/java/com/example/backsound/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.backsound
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main)
+        setCard1()
+        setCard2()
     }
+    //////////////
+
+    private fun setCard1(){
+        //Declarar MediaPlayer
+        var mp = MediaPlayer()
+        //Declaración de elementos
+        val sound1CV : CardView = findViewById(R.id.sound1)
+        //Ajustar background
+        sound1CV.setBackgroundResource(R.drawable.necoarc)
+    }
+
+    private fun setCard2(){
+        //Declarar MediaPlayer
+        var mp = MediaPlayer()
+        //Declaración de elementos
+        val sound2CV : CardView = findViewById(R.id.sound2)
+        //Ajustar background
+        sound2CV.setBackgroundResource(R.drawable.desaparecer)
+    }
+
+    /////////////
 }
